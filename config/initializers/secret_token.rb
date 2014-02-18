7e44c926430085eba18360e1d87c90c889c8b904
binadik <binadik@hotmail.com>
2014-02-18 14:51:53 +0800

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Voltaire::Application.config.secret_key_base = '8aa7688c4386be3480e8fb55ba6d50cbca5b85cef58933b43b35ce361be6ef0c119f5068ee945881b154e6bccb6d865adc651ae95abab61f500b547598fa6a75'
